edcdfe0c1c8cd55bcfd394ad38f0ee24222f165a
Leonardo Murillo <egolemur@gmail.com>
2022-10-12 06:45:20 -0700

    Commit con cambios que incluyen Intervention Img agregado con Composer y ajustes en App.php e ImageController para utilizar dependencia Intervention IMG

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImageController extends Controller
 {
     public function store(Request $request) 
     {
         $imagen = $request->file('file');
-        return response()->json(['imagen' => $imagen->extension()]);  
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000, null, 'center');
+        $imagePath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagePath);
+
+        return response()->json(['imagen' => $nombreImagen]);  
     }
 }
